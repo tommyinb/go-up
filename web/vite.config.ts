90add27a3a09923087607b748eed8c0673e59243
Tommy <tommy@tom.my>
2025-02-07 09:36:32 +0800

    small fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/solo-playground",
+  base: "./",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
